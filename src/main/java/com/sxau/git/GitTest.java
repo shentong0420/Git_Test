f8219f321436f0ae73b8a14f62a56e8dbe9323a7
shen-tongs <1295947692@qq.com>
2024-02-16 10:27:38 +0800

    first commit

---
+++ src/main/java/com/sxau/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.sxau.git;
+
+/**
+ * ClassName: gitTest
+ * Package: com.sxau.git
+ * Description:
+ *
+ * @Author: Shen
+ * @Create: 2024/2/16 - 10:24
+ * @Version: v1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
